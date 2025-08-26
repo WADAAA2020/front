2e52768ac86e2492449a27e064cd83bfa8225e09
Mateus Aparecido Gianelli Wada <44754894820@>
2025-08-26 09:05:31 -0300

    exercicios

---
+++ script.js
@@ -0,0 +1,136 @@
+// alert: serve para avisar algo para o usuário, print do python
+// alert("Hello, world")
+
+// prompt: ele é o input do python
+// prompt("Qual sua comida favorita?")
+
+// confirm: mostra 2 opçôes o "ok" e o "cancel"
+//  confirm("É sushi <3")
+
+// let: variaveis que variam, valores que flexiveis
+// const: constantes, valores fixos, como o pi(3,14)
+// var: 1 forma de declarar variaveis, códigos ligados
+
+// let materia = prompt("Qual sua matéria favorita?")
+// alert(materia)
+
+
+
+// parte 1
+// let nome = prompt("Qual é o seu nome?")
+// alert(`Olá, ${nome}`)
+
+
+
+// let materia = prompt("Qual sua matéria favorita?")
+// alert(`A matéria ${materia} realmente é ótima`)
+
+
+
+// let data = prompt("Que dia é hoje: ")
+// alert(`Hoje é dia ${data}`)
+
+
+
+// confirm("Está preparado para o segundo semestre de DS?")
+
+
+
+// parte 2 -- Number ou parseFloat
+// let nota1 = Number(prompt("Informe a primeira nota: "))
+// let nota2 = Number(prompt("Informe a segunda nota: "))
+// let media = (nota1 + nota2)/2
+
+// alert(`A sua média é ${media}`)
+
+
+
+// parte 3
+// let n1 = Number(prompt("Informe um número: "))
+// let n2 = Number(prompt("Informe outro número: "))
+
+// let soma = n1 + n2
+// let subtracao = n1 - n2
+// let multiplicacao = n1 * n2
+// let divisao = n1 / n2
+
+// alert(`A soma é ${soma}`)
+// alert(`A subtração é ${subtracao}`)
+// alert(`A multiplicação é ${multiplicacao}`)
+// alert(`A divisão é ${divisao}`)
+
+
+
+// let nota1 = Number(prompt("Informe a nota 1: "))
+// let nota2 = Number(prompt("Informe a nota 2: "))
+// let nota3 = Number(prompt("Informe a nota 3: "))
+// let media = (nota1 + nota2 + nota3)/3
+
+// alert(`A média é ${media}`)
+
+
+
+// let largura = Number(prompt("Informe a largura da parede: "))
+// let altura = Number(prompt("Informe a altura da parede: "))
+// let area = largura * altura
+// let tinta = area/2
+
+// alert(`Serão necessários ${tinta} litros de tinta para pintar a parede`)
+
+
+
+// parte 4
+// let idade = Number(prompt("Informe sua idade:"))
+
+// if (idade >= 18) {
+//     console.log("Maior de idade")
+// } else {
+//     console.log("Menor de idade")
+// }
+
+
+
+// let n1 = Number(prompt("Informe um número inteiro:"))
+
+// if (n1 % 2 === 0) {
+//     console.log("Par")
+// } else {
+//     console.log("Ímpar")
+// }
+
+
+
+// let n1 = Number(prompt("Informe número 1:"))
+// let n2 = Number(prompt("Informe número 2:"))
+
+// if (n1 > n2) {
+//     console.log("Número 1 é maior")
+// } else if (n2 > n1) {
+//     console.log("Número 2 é maior")
+// } else {
+//     console.log("São iguais")
+// }
+
+
+
+// let senha = "jsélegal"
+
+// let senhauser = prompt("Digite a senha:")
+
+// if (senha === senhauser) {
+//     console.log("Acesso permitido")
+// } else {
+//     console.log("Acesso negado")
+// }
+
+
+
+// let temperatura = Number(prompt("Informe a temperatura atual:"))
+
+// if (temperatura < 18) {
+//     console.log("Frio")
+// } else if (temperatura >= 18 || temperatura <= 25) {
+//     console.log("Agradável")
+// } else {
+//     console.log("Quente")
+// }
